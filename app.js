eaec69fc2d1c848054287c96c1773e87685906ce
alexdev0ver <salaris9315@gmail.com>
2021-01-04 20:12:27 +0700

    add bodyparser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const Question = require('./models/Question');
 const AskedQuestion = require('./models/AskedQuestion');
@@ -12,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.bodyParser());
 
 
 async function start() {
